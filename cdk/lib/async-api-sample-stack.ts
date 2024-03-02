5435e34c0add7c63d6c7521ad8324913af19879e
uttne <uttne.mkr@gmail.com>
2024-03-02 15:50:28 +0900

    apiにログの追加と受け入れメソッドの拡充

--- cdk/lib/async-api-sample-stack.ts
+++ cdk/lib/async-api-sample-stack.ts
@@ -4,6 +4,7 @@ import { Construct } from "constructs";
 import * as apigw from "aws-cdk-lib/aws-apigatewayv2";
 import { DynamoDbOpeQueueIntegration } from "./integrations/dynamodb-ope-queue";
 // import { S3PubSubIntegration } from "./integrations/s3-pub-sub";
+import * as logs from "aws-cdk-lib/aws-logs";
 
 export class AsyncApiSampleStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,6 +12,38 @@ export class AsyncApiSampleStack extends cdk.Stack {
 
     const api = new apigw.HttpApi(this, "Api", {
       apiName: "async-api-sample--api-gw",
+    });
+
+    const logGroup = new logs.LogGroup(this, "ApiLog", {
+      retention: logs.RetentionDays.ONE_DAY,
+    });
+
+    new apigw.CfnStage(this, "ApiStage", {
+      apiId: api.apiId,
+      stageName: "dev",
+      autoDeploy: true,
+      accessLogSettings: {
+        destinationArn: logGroup.logGroupArn,
+        format: JSON.stringify({
+          requestId: "$context.requestId",
+          ip: "$context.identity.sourceIp",
+          caller: "$context.identity.caller",
+          user: "$context.identity.user",
+          requestTime: "$context.requestTime",
+          httpMethod: "$context.httpMethod",
+          resourcePath: "$context.resourcePath",
+          status: "$context.status",
+          protocol: "$context.protocol",
+          responseLength: "$context.responseLength",
+          "integration.error": "$context.integration.error",
+          "integration.integrationStatus":
+            "$context.integration.integrationStatus",
+          "integration.latency": "$context.integration.latency",
+          "integration.requestId": "$context.integration.requestId",
+          "integration.status": "$context.integration.status",
+          integrationErrorMessage: "$context.integrationErrorMessage",
+        }),
+      },
     });
 
     // const s3PubSubIntegration = new S3PubSubIntegration(
@@ -31,7 +64,12 @@ export class AsyncApiSampleStack extends cdk.Stack {
 
     api.addRoutes({
       path: "/dy-queue",
-      methods: [apigw.HttpMethod.POST, apigw.HttpMethod.GET],
+      methods: [
+        apigw.HttpMethod.POST,
+        apigw.HttpMethod.GET,
+        apigw.HttpMethod.DELETE,
+        apigw.HttpMethod.PATCH,
+      ],
       integration: dynamoDbOpeQueueIntegration.integration,
     });
   }
